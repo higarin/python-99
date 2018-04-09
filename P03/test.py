73b6f595bbd6f394b0656a225f744cbc4f230810
lulichn <daisuke.develop@gmail.com>
2018-04-09 14:45:41 +0900

    [P03] IndexError を削除

--- P03/test.py
+++ P03/test.py
@@ -3,16 +3,16 @@ import main
 
 
 class Test(unittest.TestCase):
-    def test_size_0(self):
-        with self.assertRaises(IndexError):
-            main.at([], 0)
+    # def test_size_0(self):
+    #     with self.assertRaises(IndexError):
+    #         main.at([], 0)
 
     def test_size_1(self):
         self.assertTrue(main.at([1], 1), 1)
 
-    def test_size_1_out_of_lower_limit(self):
-        with self.assertRaises(IndexError):
-            main.at([1], 0)
+    # def test_size_1_out_of_lower_limit(self):
+    #     with self.assertRaises(IndexError):
+    #         main.at([1], 0)
 
     def test_size_1_out_of_upper_limit(self):
         with self.assertRaises(IndexError):
@@ -22,9 +22,9 @@ class Test(unittest.TestCase):
         self.assertTrue(main.at([1, 2], 1), 1)
         self.assertTrue(main.at([1, 2], 2), 2)
 
-    def test_size_2_out_of_lower_limit(self):
-        with self.assertRaises(IndexError):
-            main.at([1, 2], 0)
+    # def test_size_2_out_of_lower_limit(self):
+    #     with self.assertRaises(IndexError):
+    #         main.at([1, 2], 0)
 
     def test_size_2_out_of_upper_limit(self):
         with self.assertRaises(IndexError):
@@ -35,9 +35,9 @@ class Test(unittest.TestCase):
         self.assertTrue(main.at([1, 2, 3], 2), 2)
         self.assertTrue(main.at([1, 2, 3], 3), 3)
 
-    def test_size_3_out_of_lower_limit(self):
-        with self.assertRaises(IndexError):
-            main.at([1, 2, 3], 0)
+    # def test_size_3_out_of_lower_limit(self):
+    #     with self.assertRaises(IndexError):
+    #         main.at([1, 2, 3], 0)
 
     def test_size_3_out_of_upper_limit(self):
         with self.assertRaises(IndexError):
