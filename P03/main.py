f1f3563b82a90b0a06cdf2549efa915cacd8be41
lulichn <daisuke.develop@gmail.com>
2018-04-09 14:44:38 +0900

    [P03] IndexError を削除

--- P03/main.py
+++ P03/main.py
@@ -1,7 +1,4 @@
 
 def at(elements, index):
-    if index <= 0:
-        raise IndexError
-
     return elements[index - 1]
 
--- P07/main.py
+++ P07/main.py
@@ -1,14 +1,11 @@
-def _flatten(elements):
+def flatten(elements):
     ret = []
 
     for e in elements:
         if isinstance(e, (list,)):
-            ret.extend(_flatten(e))
+            ret.extend(flatten(e))
         else:
             ret.append(e)
 
     return ret
 
-
-def flatten(x):
-    return _flatten(x)
