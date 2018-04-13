80525501c50be0cd9df4fb36df0700ab1c71f373
lulichn <daisuke.develop@gmail.com>
2018-04-13 15:24:08 +0900

    [P09] テストの修正

--- P09/test.py
+++ P09/test.py
@@ -4,7 +4,7 @@ import main
 
 class Test(unittest.TestCase):
     def test(self):
-        self.assertEqual(main.pack([]), [[]])
+        self.assertEqual(main.pack([]), [])
         self.assertEqual(main.pack([1, 2, 3]), [[1], [2], [3]])
         self.assertEqual(main.pack([1, 1, 1, 1, 2, 3, 3, 1, 1, 4, 5, 5, 5, 5]),
                          [[1, 1, 1, 1], [2], [3, 3], [1, 1], [4], [5, 5, 5, 5]])
