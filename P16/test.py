b29e17c05c0b48638f5fcf44008ad8aed3d88b17
lulichn <daisuke.develop@gmail.com>
2018-04-16 20:39:17 +0900

    [P16] Mod test

--- P16/test.py
+++ P16/test.py
@@ -5,9 +5,12 @@ import main
 
 class Test(unittest.TestCase):
     def test(self):
-        self.assertEqual(main.drop([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2), [1, 3, 5, 7, 9])
-        self.assertEqual(main.drop([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3), [1, 2, 4, 5, 7, 8, 10])
-        self.assertEqual(main.drop([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4), [1, 2, 3, 5, 6, 7, 9, 10])
+        self.assertEqual(main.drop(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], 2),
+                         ['a', 'c', 'e', 'g', 'i'])
+        self.assertEqual(main.drop(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], 3),
+                         ['a', 'b', 'd', 'e', 'g', 'h', 'j'])
+        self.assertEqual(main.drop(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], 4),
+                         ['a', 'b', 'c', 'e', 'f', 'g', 'i', 'j'])
 
 
 if __name__ == "__main__":
