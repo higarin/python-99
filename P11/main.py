4fd3a7c4e41e8177144f88f20ea1e3a6da37889d
sakamoto024 <s.sakamoto@atomitech.jp>
2019-08-06 17:49:02 +0900

    P11の修正

--- P11/main.py
+++ P11/main.py
@@ -1,23 +1,9 @@
 def encode_modified(a):
-    i = 0
-    b = []
+    from main2 import pack
+    d = pack(a)
     c = []
 
-    while len(a) > 0:
-        if len(a) == 1:
-            b.append([a[0]])
-            break
-        if a[i]== a[i + 1]:
-            i += 1
-        if len(a) == i + 1:
-            b.append(a)
-            break
-        elif a[i] != a[i + 1]:
-            b.append(a[0:i + 1])
-            del a[0:i + 1]
-            i = 0
-
-    for i in b:
+    for i in d:
         if len(i) == 1:
             c.append(i[0])
         else:
