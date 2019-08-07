daa43566de951dfa357ba6427fd0e51cd69275ba
sakamoto024 <s.sakamoto@atomitech.jp>
2019-08-07 09:54:29 +0900

    P12の修正

--- P12/main.py
+++ P12/main.py
@@ -1,12 +1,10 @@
 def decode(a):
-    j = []
+    b = []
 
     for i in a:
-        if type(i) is int:
-            j.append(i)
-        elif len(i) == 2:
-            j.append([i[1]] * i[0])
-    
-    flatten = lambda x: [z for y in x for z in (flatten(y) if hasattr(y, '__iter__') and not isinstance(y, str) else (y,))]
-    return flatten(j)
+        if type(i) is list:
+            b += [i[1] for y in range(i[0])]
+        elif type(i) is int:
+            b.append(i)
 
+    return b
