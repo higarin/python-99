4ba50ceb6d435586582f34d8528e23415959a266
sakamoto024 <s.sakamoto@atomitech.jp>
2019-08-08 15:27:03 +0900

    P24の修正(listの削除と内包表記追加)

--- P24/main.py
+++ P24/main.py
@@ -1,8 +1,10 @@
 import random
-result = []
 
 def rnd_select(a,b):
+    result = []
     for i in range(a):
-        result.append(random.choice(list(range(b))))
+        result.append(random.choice((range(b+1))))
 
     return result
+
+    #return [random.choice(range(b+1)) for i in range(a)]
