9d1025138045c044b05f4853a941ab70d2c94439
sakamoto024 <s.sakamoto@atomitech.jp>
2019-08-23 10:30:17 +0900

    P31の修正

--- P31/main.py
+++ P31/main.py
@@ -1,10 +1,8 @@
 def is_prime(num):
-    if num == 0:
+    if num == 0 or num == 1:
         return False
-    elif num == 1:
-        return False
-    else:#2以降の素数を処理
-        for i in range(2,num):
-            if num % i == 0:
-                return False
-        return True
+    
+    for i in range(2,num):
+        if num % i == 0:
+            return False
+    return True#
