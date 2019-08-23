018c8a5dd7eca8d5eb6d18aa43f6047e9d00a668
sakamoto024 <s.sakamoto@atomitech.jp>
2019-08-23 14:28:59 +0900

    P32の修正

--- P32/main.py
+++ P32/main.py
@@ -1,3 +1,11 @@
 def gcd(num1,num2):
-    import math
-    return math.gcd(num1,num2)
+    while num2 != 0:
+      num1,num2 = num2,num1%num2
+     ###上記分からなかったので、使い方覚えるようにする###
+    return num1
+
+#gcd(36,63)
+#63 36
+#36 27
+#27 9
+#9 0
