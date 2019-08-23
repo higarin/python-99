97e02d2971395f3ef057b40bf7e2a8433126a7fb
sakamoto024 <s.sakamoto@atomitech.jp>
2019-08-23 18:05:50 +0900

    P35の修正

--- P35/main.py
+++ P35/main.py
@@ -1,16 +1,10 @@
 def prime_factors(num):
-    prime_num = [2,3,5,7]
-    i = 0
     result = []
-
-    while num > 1:
-        if num % prime_num[i] == 0:
-            num = int(num / prime_num[i])
-            result.append(prime_num[i])
-        else:
-            i += 1
-            if i == 4:#[2,3,5,7]で割り切れなかった場合
-                result.append(2)
-                num = int(num / 2)
-                i = 0
+    for i in range(2,num):
+        while num % i == 0:
+            num //= i
+            result.append(i)
+        if i * i >= num:
+            result.append(num)
+            break
     return result
