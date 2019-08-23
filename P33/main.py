a73511bf45e3b6ff8bb84fe54aafb83d5ceb9e60
sakamoto024 <s.sakamoto@atomitech.jp>
2019-08-23 14:30:05 +0900

    P33の修正

--- P33/main.py
+++ P33/main.py
@@ -1,8 +1,11 @@
-import math
+import sys
+sys.path.append('../')
+from P32.main import gcd
 
 def is_coprime(num1,num2):
-    gcd = math.gcd(num1,num2)
-    if gcd == 1:
+    gcd_num = gcd(num1,num2)
+    if gcd_num == 1:
         return True
     else:
         return False
+    
